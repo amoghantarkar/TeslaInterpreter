11235b2c698290bddc295a0289dec72906bd72ca
renukajoshi <renujoshi.94@gmail.com>
2020-03-09 02:23:05 -0500

    JUnit test for FileUtil.java

---
+++ src/test/java/com/tesla/energy/file/FileUtilTest.java
@@ -0,0 +1,36 @@
+package com.tesla.energy.file;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FileUtilTest {
+
+  @Test
+  void verifyFilePath_validPath_returnsTrue() {
+    String directory = System.getProperty("user.home");
+    boolean result = FileUtil.verifyFilePath(directory);
+    assertTrue(result);
+  }
+
+  @Test
+  void verifyFilePath_invalidPath_returnsFalse() {
+    String directory = "/unknown/directory";
+    boolean result = FileUtil.verifyFilePath(directory);
+    assertFalse(result);
+  }
+
+  @Test
+  void verifyFileDirectory_validPath_returnsTrue() {
+    String directory = System.getProperty("user.home");
+    boolean result = FileUtil.verifyFileDirectory(directory);
+    assertTrue(result);
+  }
+
+  @Test
+  void verifyFileDirectory_invalidPath_returnsFalse() {
+    String directory = "/unknown/directory";
+    boolean result = FileUtil.verifyFileDirectory(directory);
+    assertFalse(result);
+  }
+}
